b92e01f5a2ac5a2fdde5f358108e449754e54d7b
Chen Zhen2 <ChenZhen2@aactechnologies.com>
2020-07-31 17:03:49 +0800

    初始化

---
+++ src/test/java/org/daistudy/min/MinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daistudy.min;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
